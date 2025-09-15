e6bcdd9c7a4eb6fe23a6c312bdf0af914b795ef9
regilanmeira <regilan@hotmail.com>
2025-09-15 20:13:15 -0300

    Término dos formulários

--- index.php
+++ index.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 
-    <?php
-       include "referencias.php";
-    ?>
+  <?php
+  include "referencias.php";
+  ?>
 </head>
+
 <body>
-<main class="container-fluid">
-
-<div class="row">
-<div class="col-sm-12">
-  <a href="tarefa_marcar.php"><button type="button" class="btn btn-primary">Marcar tarefa</button></a>
-<p>Regilan</p>
-</div>
+  <main class="bg-light">
 
 
-</div>
-
-
-</main>
+          <div class="item bg-gradient">
+            <a href="tarefa_marcar.php">Marcar tarefa</a>
+          </div>
+          <div class="item bg-gradient">
+            <a href="tarefa_listar.php">Listar tarefas</a>
+          </div>
+          <div class="item bg-gradient">
+            <a href="tarefa_pesquisar.php">Pesquisar tarefas</a>
+          </div>
     
+
+  </main>
+
 </body>
+
 </html>
